a9929cc767960cfd269a90b94dfdb14536b81e6d
yash-162 <yashspatel86@gmail.com>
2022-06-26 12:14:17 +0530

    JS commit

---
+++ object.js
@@ -0,0 +1,12 @@
+//Object
+let student = {
+    name: 'yash',
+    number: 29,
+    age: 21,
+    class: 'B'
+};
+//Dot Notation
+student.marks = 89;
+//Print 
+
+console.log(student.name + '   ' + student.age);
---
+++ variable.js
@@ -0,0 +1,5 @@
+//String
+let name = 'yash', name2 = 'mihir';
+console.log('My name is:' + name);
+console.log('My brother name is:' + name2)
+//
